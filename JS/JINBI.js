62c15a604d380d9133a2e657f9291f511f92bb90
Kpobw <921732268@qq.com>
2024-08-25 16:49:50 +0800

    Add files via upload

---
+++ JS/JINBI.js
@@ -0,0 +1,11 @@
+
+ function numAdd() {
+	var num = document.getElementById('addtext');
+	num.innerHTML++;
+	//document.cookie=num.value;
+}
+function numRed() {
+	var num = document.getElementById('addtext');
+	num.innerHTML--;
+	//document.cookie=num.value;
+}
